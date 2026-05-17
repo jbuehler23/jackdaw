dfb4f8dea7188e3908339b3ba02cb410f6a55b02
Wells Crosby <64884461+wellscrosby@users.noreply.github.com>
2026-05-17 10:37:06 -0700

    Add Bevy icon to window (#241)
    * Add bevy icon to winit window
    
    * add icon and window_icon.rs
    
    * Delete editor_window_icon.rs
    
    * fix clippy warning
    
    * added comments clarifying that its only windows and x11
    
    * Change icon to emoji based one to not violate bevy trademark
    
    * update to @hadalade design, add image to readme, use downscaled for icon
    
    * update icon to have one blueprint only

---
+++ src/window_icon.rs
@@ -0,0 +1,68 @@
+use bevy::{ecs::system::NonSendMarker, prelude::*, window::WindowCreated, winit::WINIT_WINDOWS};
+use winit::window::Icon;
+
+const WINDOW_ICON_PNG: &[u8] = include_bytes!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/assets/jackdaw_icon_small.png"
+));
+
+#[derive(Resource)]
+struct WindowIconResource(Option<Icon>);
+
+/// Adds a bevy icon to the winit window.
+/// Note that this only works on Windows and x11 due to limitations of winit.
+pub(crate) fn install(app: &mut App) {
+    app.insert_resource(WindowIconResource(load_icon_png()));
+    app.add_systems(PostUpdate, apply_window_icon_on_window_created);
+}
+
+fn load_icon_png() -> Option<Icon> {
+    let image = match image::load_from_memory(WINDOW_ICON_PNG) {
+        Ok(image) => image.into_rgba8(),
+        Err(error) => {
+            bevy::log::warn_once!(
+                "jackdaw: failed to decode embedded window icon PNG: {:?}",
+                error
+            );
+            return None;
+        }
+    };
+    let width = image.width();
+    let height = image.height();
+    let rgba = image.into_raw();
+    let icon = match Icon::from_rgba(rgba, width, height) {
+        Ok(icon) => icon,
+        Err(error) => {
+            bevy::log::warn_once!(
+                "jackdaw: failed to create window icon from embedded PNG: {:?}",
+                error
+            );
+            return None;
+        }
+    };
+    Some(icon)
+}
+
+fn apply_window_icon_on_window_created(
+    _main_thread: NonSendMarker,
+    icon_state: Res<WindowIconResource>,
+    mut created: MessageReader<WindowCreated>,
+) {
+    let Some(icon) = icon_state.0.as_ref() else {
+        return;
+    };
+    for event in created.read() {
+        WINIT_WINDOWS.with(|windows_cell| {
+            let winit_windows = windows_cell.borrow();
+            let Some(backend_window) = winit_windows.get_window(event.window) else {
+                bevy::log::warn_once!(
+                    "jackdaw: winit backend window missing when applying decoration icon ({:?}); ignoring",
+                    event.window,
+                );
+                return;
+            };
+            // This only works on Windows and x11.
+            backend_window.set_window_icon(Some(icon.clone()));
+        });
+    }
+}
